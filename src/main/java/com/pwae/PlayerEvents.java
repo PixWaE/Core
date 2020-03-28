062728076c35eb03e9c0d45dbee681c398b26d12
BuildTools <lochlanpainter@gmail.com>
2020-03-28 20:50:08 +1100

    Update PlayerEvents.java
    It actually works this time.

--- src/main/java/com/pwae/PlayerEvents.java
+++ src/main/java/com/pwae/PlayerEvents.java
@@ -3,22 +3,16 @@ package com.pwae;
 //Imports
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.util.text.TextFormatting;
-import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.entity.player.EntityPlayerMP;
 
 public class PlayerEvents {
   private final String PREFIX = TextFormatting.GOLD + "-> ";
   
   @SubscribeEvent
   //Send Modpack information when joining a world
-  public void layerJoin(EntityJoinWorldEvent event) {
-      if (!(event.getEntity() instanceof EntityPlayer))
-          return;
-      EntityPlayer player = (EntityPlayer)event.getEntity();
-	  player.sendStatusMessage(new TextComponentString(TextFormatting.WHITE + "" + TextFormatting.BOLD + "Welcome to PWaE 0.3.0d"), false);
-	  player.sendStatusMessage(new TextComponentString(TextFormatting.WHITE + "" + TextFormatting.BOLD + "This version is a test. Expect bugs, crashes, etc. to be present"), false);
+  public void playerJoin(PlayerEvent.PlayerLoggedInEvent event) {
+	  event.player.sendStatusMessage(new TextComponentString(TextFormatting.WHITE + "" + TextFormatting.BOLD + "Welcome to PWaE 0.3.0d"), false);
+	  event.player.sendStatusMessage(new TextComponentString(TextFormatting.WHITE + "" + TextFormatting.BOLD + "This version is a test. Expect bugs, crashes, etc. to be present"), false);
   }
 }
